1f647cddc1bc5ac870d5ecc87dedaf5aa667af6a
cknoettg <cknoettg@yahoo.com>
2020-04-23 23:56:14 -0400

    completed data set

---
+++ src/data.js
@@ -0,0 +1,18 @@
+export default [
+{
+    id: 1,
+    name: "Nickolai G-Unit",
+},
+{
+    id: 2,
+    name: "Corey CK Dub",
+},
+{
+    id: 3,
+    name: "Michael Cloudy",
+},
+{
+    id: 4,
+    name: "Michael RazorBlades",
+}
+];
